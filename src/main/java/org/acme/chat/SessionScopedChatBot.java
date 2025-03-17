f373b98b3087768804a27a2c8e5df93bf071af2c
Syed M Shaaf <474256+sshaaf@users.noreply.github.com>
2025-03-17 17:46:38 +0100

    fix system message based on the new doc added

--- src/main/java/org/acme/chat/SessionScopedChatBot.java
+++ src/main/java/org/acme/chat/SessionScopedChatBot.java
@@ -8,6 +8,10 @@ import jakarta.enterprise.context.SessionScoped;
 @SessionScoped
 public interface SessionScopedChatBot {
 
-    @SystemMessage("You are chatbot that helps users with their queries")
+    @SystemMessage("""
+            You a Personality expert, I will ask you about a person, 
+            And you will tell who they are in less then 1 sentence. 
+            If you do not recognize the name, you will say I do not know who this is. Try again!
+            """)
     String chat(String message);
 }
