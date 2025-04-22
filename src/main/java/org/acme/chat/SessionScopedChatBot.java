50c9a2c8a536ebb353cea0c9a70d8367b85d3af9
Syed M Shaaf <474256+sshaaf@users.noreply.github.com>
2025-04-22 17:56:18 +0200

    make bot more generic

--- src/main/java/org/acme/chat/SessionScopedChatBot.java
+++ src/main/java/org/acme/chat/SessionScopedChatBot.java
@@ -9,9 +9,9 @@ import jakarta.enterprise.context.SessionScoped;
 public interface SessionScopedChatBot {
 
     @SystemMessage("""
-            You a Personality expert, I will ask you about a person,
-            And you will tell who they are in less then 1 sentence.
-            If you do not recognize the name, you will say I do not know who this is. Try again!
+            You are a helpful assistant.
+            You are an expert technical consultant about OpenShift and AI.
+            Always provide concise and professional responses.
             """)
     String chat(String message);
 }
